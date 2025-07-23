1ba97170ee8b527465cb96c4df55a0cc5d2770b0
LanXuelin <779822707@qq.com>
2025-07-23 10:44:00 +0800

    测试分支

---
+++ Chapter1/Ex1_01.cpp
@@ -0,0 +1,13 @@
+#include <iostream> //书中import <iostream>;语句在mingw15.1.0中会报错,需改用旧方法包含头文件
+
+int main()
+{
+    int answer{42};
+    std::cout << "The answer to life, the universe, and everything is "
+              << answer
+              << std::endl;
+    std::cout << "ni hao ma?" << std::endl;
+    std::cin.get(); // 解决独立CMD运行时窗口一闪而过的问题
+
+    return 0;
+}
---
+++ Chapter1/Ex1_02.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    int answer{42};
+    std::cout << "Hello World! The answer is " << answer << std::endl;
+    return 0;
+}
