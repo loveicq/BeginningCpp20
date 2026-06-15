19d1dc3e4644e0e45ba629c019f783d7e764ca39
LanXuelin <779822707@qq.com>
2026-06-15 23:30:13 +0800

    20260615

---
+++ Chapter11/Ex11_03/Ex11_03.cpp
@@ -0,0 +1,20 @@
+import<iostream>;
+import roman;
+
+int main()
+{
+	std::cout << "MMXX in Arabic numerals is " << from_roman("MMXX") <<std::endl;
+	std::cout << "1234 in Roman numerals is " << to_roman(1234).data() << std::endl;
+	std::cout << "This consists of " << to_roman(1234).size() << " numerals" << std::endl;
+
+	auto roman{ to_roman(567) };
+	std::cout << "567 in Roman numerals is " << roman.c_str() << std::endl;
+
+	//to_roman(1234).data()、to_roman.c_str()和to_roman(1234)三者的区别是什么？
+	//教材中的to_roman(1234).c_data()是错的，没有这个成员函数
+
+	//教材说没有import<string>;的话此行会出错，但我测试了没有问题，可能是编译器的原因
+	std::cout << "1234 in Roman numerals is " << to_roman(1234) << std::endl;
+
+	std::cin.get();
+}
---
+++ Chapter11/Ex11_03/roman.ixx
@@ -0,0 +1,6 @@
+export module roman;
+import<string>;
+import<string_view>;
+
+export std::string to_roman(unsigned i);
+export unsigned from_roman(std::string_view roman);
