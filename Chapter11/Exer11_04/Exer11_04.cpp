437d63ad9b329805494519fc7c50bb95a13922d2
LanXuelin <779822707@qq.com>
2026-06-25 22:18:30 +0800

    20260625

---
+++ Chapter11/Exer11_04/Exer11_04.cpp
@@ -0,0 +1,23 @@
+import<string>;
+import<iostream>;
+import words;
+
+int main()
+{
+	words::Words the_words;
+	std::string text;
+	const auto separators{ " ,.!?\"\n" };
+
+	std::cout << "Enter a string terminated by *:" << std::endl;
+	getline(std::cin, text, '*');
+
+	words::extract_words(the_words, text, separators);
+	if (the_words.empty())
+	{
+		std::cout << "No words in text." << std::endl;
+		return 0;
+	}
+
+	words::sort(the_words);
+	words::show_words(the_words);
+}
---
+++ Chapter11/Exer11_04/words.cpp
@@ -0,0 +1,65 @@
+module words;
+
+import<iostream>;
+import<format>;
+import :internals;
+
+void words::extract_words(Words& words, const std::string& text, const std::string& separators)
+{
+	size_t start{ text.find_first_not_of(separators) };
+	while (start != std::string::npos)
+	{
+		size_t end{ text.find_first_of(separators,start + 1) };
+		if (end == std::string::npos)
+			end = text.length();
+		words.push_back(std::make_shared<std::string>(text.substr(start, end - start)));
+		start = text.find_first_not_of(separators, end + 1);
+	}
+}
+
+void sort(words::Words& words, size_t start, size_t end);
+
+void words::sort(Words& words)
+{
+	if (!words.empty())
+		::sort(words, 0, words.size() - 1);
+}
+
+void sort(words::Words& words, size_t start, size_t end)
+{
+	if (!(start < end))
+		return;
+
+	swap(words, start, (start + end) / 2);
+
+	size_t current{ start };
+	for (size_t i{ start + 1 }; i <= end; i++)
+	{
+		if (*words[i] < *words[start])
+			swap(words, ++current, i);
+	}
+
+	swap(words, start, current);
+
+	if (current > start) sort(words, start, current - 1);
+	if (end > current + 1) sort(words, current + 1, end);
+}
+
+void words::show_words(const Words& words)
+{
+	const size_t field_width{ max_word_length(words) + 1 };
+	const size_t words_per_line{ 8 };
+	std::cout << std::format("{:{}}", *words[0], field_width);
+
+	size_t words_in_line{};
+	for (size_t i{ 1 }; i < words.size(); ++i)
+	{
+		if ((*words[i])[0] != (*words[i - 1])[0] || ++words_in_line == words_per_line)
+		{
+			words_in_line = 0;
+			std::cout << std::endl;
+		}
+		std::cout << std::format("{:{}}", *words[i], field_width);
+	}
+	std::cout << std::endl;
+}
