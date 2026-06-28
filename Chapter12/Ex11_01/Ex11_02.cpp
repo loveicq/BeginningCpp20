dd62e72dc4959ce32262c74e8c925d51035474c5
LanXuelin <779822707@qq.com>
2026-06-28 23:41:16 +0800

    20260628

--- Chapter12/Ex11_01/Ex11_02.cpp
+++
@@ -1,9 +0,0 @@
-import <iostream>;
-import <string>;
-import roman;
-
-int main()
-{
-	std::cout << "1234 in Roman numerals is " << to_roman(1234) << std::endl;
-	std::cout << "MMXX in Arabic numerals is " << from_roman("MMXX") << std::endl;
-}
--- Chapter12/Ex11_01/roman.ixx
+++
@@ -1,7 +0,0 @@
-// roman.ixx - Interface file for a Roman numerals module
-export module roman;
-import <string>;
-import <string_view>;
-
-export std::string to_roman(unsigned i);
-export unsigned from_roman(std::string_view roman);
