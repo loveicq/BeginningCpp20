634fed7fa63e1bc79b5a6d390cc37d777ffb4125
LanXuelin <779822707@qq.com>
2026-06-30 17:27:58 +0800

    20260630

---
+++ Chapter12/Ex12_01/Ex12_01.cpp
@@ -0,0 +1,9 @@
+// Ex12_01.cpp
+#include <iostream>
+import math;
+
+int main()
+{
+    std::cout << "Hello!" << std::endl;
+    std::cout << "平方结果是：" << square(1.33) << std::endl;
+}
---
+++ Chapter12/Ex12_01/math.cppm
@@ -0,0 +1,7 @@
+// math.cppm
+export module math;
+
+export auto square(const auto &x)
+{
+    return x * x;
+}
